605ca7f5fcebb64360b35c8d2c97bc7add810342
Baisangur-Dudayev <113896223+Baisangur-Dudayev@users.noreply.github.com>
2023-11-09 21:15:52 +0100

    Update main.py

--- myproject/main.py
+++ myproject/main.py
@@ -1,11 +1,18 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+#OAuth
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import auth
 
 import crud
 import models
 import schemas
 from database import SessionLocal, engine
 import os
+
+
+
+
 
 if not os.path.exists('.\sqlitedb'):
     os.makedirs('.\sqlitedb')
@@ -23,6 +30,27 @@ def get_db():
         yield db
     finally:
         db.close()
+#OAuth
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@app.post("/token")
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    #Try to authenticate the user
+    user = auth.authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # Add the JWT case sub with the subject(user)
+    access_token = auth.create_access_token(
+        data={"sub": user.email}
+    )
+    #Return the JWT as a bearer token to be placed in the headers
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
 
 #AUTHORS
 @app.post("/authors/", response_model=schemas.Author)
@@ -34,13 +62,13 @@ def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/authors/", response_model=list[schemas.Author])
-def read_authors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_authors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     authors = crud.get_authors(db, skip=skip, limit=limit)
     return authors
 
 
 @app.get("/authors/{author_id}", response_model=schemas.Author)
-def read_author(author_id: int, db: Session = Depends(get_db)):
+def read_author(author_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     db_author = crud.get_author(db, author_id=author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author not found")
@@ -48,9 +76,7 @@ def read_author(author_id: int, db: Session = Depends(get_db)):
 
 #BOOKS
 @app.post("/authors/{author_id}/books/", response_model=schemas.Book)
-def create_book_for_author(
-    author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)
-):
+def create_book_for_author(author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     return crud.create_author_book(db=db, book=book, author_id=author_id)
 
 @app.get("/books/", response_model=list[schemas.Book])
@@ -59,7 +85,7 @@ def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return books
 
 @app.delete("/books/{book_id}", response_model=schemas.Book)
-def delete_book_for_author(book_id: int, db: Session = Depends(get_db)):
+def delete_book_for_author(book_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     book = crud.delete_book(db=db, book_id=book_id)
     if book:
         return book
@@ -67,13 +93,11 @@ def delete_book_for_author(book_id: int, db: Session = Depends(get_db)):
 
 #PENNAMES
 @app.post("/authors/{author_id}/pen_names/",response_model=schemas.PenName)
-def create_pen_name_for_author(
-    author_id: int, pen_name: schemas.PenNameCreate, db:Session = Depends(get_db)
-):
+def create_pen_name_for_author(author_id: int, pen_name: schemas.PenNameCreate, db:Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     return crud.create_author_pen_name(db=db, pen_name=pen_name, author_id=author_id)
 
 @app.delete("/pen_names/{pen_name_id}", response_model=schemas.PenName)
-def delete_pen_name(pen_name_id: int, db: Session = Depends(get_db)):
+def delete_pen_name(pen_name_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     pen_name = crud.delete_pen_name(db=db, pen_name_id=pen_name_id)
     if pen_name:
         return pen_name
