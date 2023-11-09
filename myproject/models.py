edacc2c65aa1100d472b1332caebad891700abcd
Baisangur-Dudayev <113896223+Baisangur-Dudayev@users.noreply.github.com>
2023-11-09 18:58:52 +0100

    Update models.py

--- myproject/models.py
+++ myproject/models.py
@@ -4,23 +4,47 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 
-class User(Base):
-    __tablename__ = "users"
+class Author(Base):
+    __tablename__ = "authors"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    first_name = Column(String)
+    last_name = Column(String, index=True)
+    place_of_birth = Column(String)
+    biography = Column(String)
     is_active = Column(Boolean, default=True)
 
-    items = relationship("Item", back_populates="owner")
+    # Define the relationship with pen names
+    pen_names = relationship("PenName", back_populates="author2")
+
+    # Define the relationship with books
+    books = relationship("Book", back_populates="author")
 
 
-class Item(Base):
-    __tablename__ = "items"
+class Book(Base):
+    __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    description = Column(String)
+    publication_date = Column(String)
+    genre = Column(String)
+    #FK
+    author_id = Column(Integer, ForeignKey("authors.id"))
 
-    owner = relationship("User", back_populates="items")
+    # Define the relationship with the author
+    author = relationship("Author", back_populates="books")
+
+
+class PenName(Base):
+    __tablename__ = "pen_names"
+
+    id = Column(Integer, primary_key=True, index=True)
+    pen_name = Column(String, index=True)
+    #FK
+    author_id = Column(Integer, ForeignKey("authors.id"))
+
+    # Define the relationship with the author
+    author2 = relationship("Author", back_populates="pen_names")
