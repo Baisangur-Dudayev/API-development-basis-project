c8bfe48ba9a935902f11df4aff59acfd4b70e058
Baisangur-Dudayev <113896223+Baisangur-Dudayev@users.noreply.github.com>
2023-11-09 18:57:09 +0100

    Update main.py

--- myproject/main.py
+++ myproject/main.py
@@ -24,37 +24,58 @@ def get_db():
     finally:
         db.close()
 
-
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
+#AUTHORS
+@app.post("/authors/", response_model=schemas.Author)
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+    db_author = crud.get_author_by_email(db, email=author.email)
+    if db_author:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    return crud.create_author(db=db, author=author)
 
 
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+@app.get("/authors/", response_model=list[schemas.Author])
+def read_authors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    authors = crud.get_authors(db, skip=skip, limit=limit)
+    return authors
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+@app.get("/authors/{author_id}", response_model=schemas.Author)
+def read_author(author_id: int, db: Session = Depends(get_db)):
+    db_author = crud.get_author(db, author_id=author_id)
+    if db_author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return db_author
 
-
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+#BOOKS
+@app.post("/authors/{author_id}/books/", response_model=schemas.Book)
+def create_book_for_author(
+    author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)
 ):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
+    return crud.create_author_book(db=db, book=book, author_id=author_id)
 
+@app.get("/books/", response_model=list[schemas.Book])
+def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    books = crud.get_books(db, skip=skip, limit=limit)
+    return books
 
-@app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
+@app.delete("/books/{book_id}", response_model=schemas.Book)
+def delete_book_for_author(book_id: int, db: Session = Depends(get_db)):
+    book = crud.delete_book(db=db, book_id=book_id)
+    if book:
+        return book
+    raise HTTPException(status_code=404, detail="Book not found")
+
+#PENNAMES
+@app.post("/authors/{author_id}/pen_names/",response_model=schemas.PenName)
+def create_pen_name_for_author(
+    author_id: int, pen_name: schemas.PenNameCreate, db:Session = Depends(get_db)
+):
+    return crud.create_author_pen_name(db=db, pen_name=pen_name, author_id=author_id)
+
+@app.delete("/pen_names/{pen_name_id}", response_model=schemas.PenName)
+def delete_pen_name(pen_name_id: int, db: Session = Depends(get_db)):
+    pen_name = crud.delete_pen_name(db=db, pen_name_id=pen_name_id)
+    if pen_name:
+        return pen_name
+    raise HTTPException(status_code=404, detail="Pen Name not found")
+
