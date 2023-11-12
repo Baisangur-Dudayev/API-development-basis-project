9cadae5a498121f5ccebf447123c450c00a4537f
Baisangur-Dudayev <113896223+Baisangur-Dudayev@users.noreply.github.com>
2023-11-12 14:57:23 +0100

    Update auth.py

--- myproject/auth.py
+++ myproject/auth.py
@@ -16,8 +16,8 @@ from datetime import datetime, timedelta
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = os.environ.get("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")#
-#ACCESS_TOKEN_EXPIRE_MINUTES = 30
+#ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")#
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 #HASHING
 def get_password_hash(password):
