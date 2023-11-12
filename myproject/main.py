435a476990291319bcd0a0f8310e85dbd964f214
Baisangur-Dudayev <113896223+Baisangur-Dudayev@users.noreply.github.com>
2023-11-12 21:10:12 +0100

    Update main.py

--- myproject/main.py
+++ myproject/main.py
@@ -70,7 +70,7 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
     #Return the JWT as a bearer token to be placed in the headers
     return {"access_token": access_token, "token_type": "bearer"}
 
-@app.get("/users/me", response_model=schemas.User)
+@app.get("/users/me", response_model=schemas.Author)
 def read_users_me(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     current_user = auth.get_current_active_user(db, token)
     return current_user
