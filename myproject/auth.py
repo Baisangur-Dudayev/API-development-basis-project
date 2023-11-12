5d66d46818c78939c628dbb6a82fd4e9987cd083
Baisangur-Dudayev <113896223+Baisangur-Dudayev@users.noreply.github.com>
2023-11-12 21:08:37 +0100

    Update auth.py

--- myproject/auth.py
+++ myproject/auth.py
@@ -1,5 +1,4 @@
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-import auth
+
 
 import os #voor github secrets
 
@@ -19,7 +18,6 @@ from datetime import datetime, timedelta
 #SECRET_KEY = os.environ.get("SECRET_KEY")
 #ALGORITHM = os.environ.get("ALGORITHM")
 #ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
