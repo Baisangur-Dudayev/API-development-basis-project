54e450dc2716ab68ea7b4095d67e4c63fb77319a
Baisangur-Dudayev <113896223+Baisangur-Dudayev@users.noreply.github.com>
2023-11-12 20:45:53 +0100

    Update auth.py

--- myproject/auth.py
+++ myproject/auth.py
@@ -54,3 +54,26 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+def get_current_user(db: Session, token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = crud.get_user_by_email(db, username)
+    if user is None:
+        raise credentials_exception
+    return user
+    
+def get_current_active_user(db: Session, token: str = Depends(oauth2_scheme)):
+    current_user = get_current_user(db, token)
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
